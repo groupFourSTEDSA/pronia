9cc0042fc55ac2069b0a3a5ac1ecf32937c56520
EddyJCuesta <cuesta79@gmail.com>
2023-06-11 21:51:34 -0400

    increase number to cart added to firebase

--- app.js
+++ app.js
@@ -10,34 +10,42 @@ import {getDatabase, ref, set} from 'https://www.gstatic.com/firebasejs/9.22.2/f
 
 
 // Increase the cart count
+// Target the add to cart button using QuerySelector
 const cartIcons = document.querySelectorAll(".iconBox");
-
-// Initialize the cart count
+// create an empty array for the cart
 let count = 0;
-
 // Attach a click event listener to each cart icon
 cartIcons.forEach((cartIcon) => {
-     // create an EventListener to listen for when the add to cart button is clicked, the cart number increases on the page and update the database on Firebase
+     // create an EventListener to listen for when the add to cart button is clicked, the cart number increases on the page
   cartIcon.addEventListener("click", () => {
-    // Increment the count
+    // function to increase the cart count
     count++;
-
     // Target the empty cart button using querySelectorbyID
     // Update the cart number text
     const cartNumber = document.getElementById("cartNumber");
+    // append the increased number to the page
     cartNumber.textContent = count;
-
+     // Update the count in Firebase
+     updateFirebaseCartCount(count);
   });
 });
+    // Function to update the count in Firebase
+    // use the button Id to create the reference in the database
+    function updateFirebaseCartCount(count) {
+    // Get a reference to the Firebase Realtime Database
+    const database = getDatabase();
+  
+    // Set the count value in the "cart" node
+    const cartRef = ref(database, "cart/cartNumber");
+    set(cartRef, count)
+      .then(() => {
+        console.log("Cart count updated in Firebase");
+      })
+      .catch((error) => {
+        console.error("Error updating cart count in Firebase:", error);
+      });
+  }
  
-
-
- // use the button Id to create the reference in the database
- // Target the add to cart button using QuerySelector
- // create an empty array for the cart
- // function to increase the cart count
- // append the increased number to the page
-
 //  Implement a search bar that allows user to search for products by name 
 // Create an input element for the search(searchInput) in the html
  // Create a search button (searchButton)
