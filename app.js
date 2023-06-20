80bcdcf421478f8e455d756693ca5ed2f63cc087
EddyJCuesta <cuesta79@gmail.com>
2023-06-19 20:58:45 -0400

    merged conflict

--- app.js
+++ app.js
@@ -11,43 +11,45 @@ const dbRef = ref(database);
   // Link Firebase to apps.js in our code
   // Initialize the app for web usage and get the config info
 
-  // Increase the cart count
-  // Target the add to cart button using QuerySelector
-  const cartIcons = document.querySelectorAll(".iconBox");
-  // create an empty array for the cart
-  let count = 0;
-  // Attach a click event listener to each cart icon
-  cartIcons.forEach((cartIcon) => {
-      // create an EventListener to listen for when the add to cart button is clicked, the cart number increases on the page
-  cartIcon.addEventListener("click", () => {
-      // function to increase the cart count
-      count++;
-      // Target the empty cart button using querySelectorbyID
-      // Update the cart number text
-      const cartNumber = document.getElementById("cartNumber");
-      // append the increased number to the page
-      cartNumber.textContent = count;
-      // Update the count in Firebase
-      updateFirebaseCartCount(count);
-    });
-  });
-     // Function to update the count in Firebase
-     // use the button Id to create the reference in the database
-      function updateFirebaseCartCount(count) {
-     // Get a reference to the Firebase Realtime Database
-      const database = getDatabase();
 
-     // Set the count value in the "cart" node
-      const cartRef = ref(database, "cart/cartNumber");
-      set(cartRef, count)
-        .then(() => {
+// Increase the cart count
+// Target the add to cart button using QuerySelector
+const cartIcons = document.querySelectorAll(".iconBox");
+// create an empty array for the cart
+let count = 0;
+// Attach a click event listener to each cart icon
+cartIcons.forEach((cartIcon) => {
+     // create an EventListener to listen for when the add to cart button is clicked, the cart number increases on the page
+  cartIcon.addEventListener("click", () => {
+    // function to increase the cart count
+    count++;
+    // Target the empty cart button using querySelectorbyID
+    // Update the cart number text
+    const cartNumber = document.getElementById("cartNumber");
+    // append the increased number to the page
+    cartNumber.textContent = count;
+     // Update the count in Firebase
+     updateFirebaseCartCount(count);
+  });
+});
+    // Function to update the count in Firebase
+    // use the button Id to create the reference in the database
+    function updateFirebaseCartCount(count) {
+    // Get a reference to the Firebase Realtime Database
+    const database = getDatabase();
+  
+    // Set the count value in the "cart" node
+    const cartRef = ref(database, "cart/cartNumber");
+    set(cartRef, count)
+      .then(() => {
         console.log("Cart count updated in Firebase");
       })
-        .catch((error) => {
+      .catch((error) => {
         console.error("Error updating cart count in Firebase:", error);
       });
   }
  
+
 //  Implement a search bar that allows user to search for products by name 
 
 // Add an event listener to the plantSelect dropdown menu
@@ -86,23 +88,22 @@ function openPlantWindow(plantData) {
   window.open(url, '_blank');
 }
 
-
- //  Implement a ratings and review system that allows user to leave feedback and ratings on products
-  // create a carousel to hold the ratings and comments
-  // select all slides
- // loop through slides and set each slides translateX property to index * 100% // current slide counter
-  // maximum number of slides
-  // select next slide button
- // add an eventListener and next slide functionality
-  // check if current slide is the last and reset the current slide // move slide by -100%
-  // select previous slide button
-  // add an EventListener and next slide functionality
-  // check if current slide is the last an reset current slide
- // move slide by -100%
- // create HTML form to create a review and rating form // target the form with getElementById
-  // target the submit button getElementById
-  // event.preventDefault();
- // create a container through html to hold the review and ratings // style the container to that holds the reviews and ratings
-  // style the rating bar?
-  // target the container using querySelector
- // store the review and rating in the database on Firebase // append the review and rating to the page
+//  Implement a ratings and review system that allows user to leave feedback and ratings on products
+ // create a carousel to hold the ratings and comments
+ // select all slides
+// loop through slides and set each slides translateX property to index * 100% // current slide counter
+ // maximum number of slides
+ // select next slide button
+// add an eventListener and next slide functionality
+ // check if current slide is the last and reset the current slide // move slide by -100%
+ // select previous slide button
+ // add an EventListener and next slide functionality
+ // check if current slide is the last an reset current slide
+// move slide by -100%
+// create HTML form to create a review and rating form // target the form with getElementById
+ // target the submit button getElementById
+ // event.preventDefault();
+// create a container through html to hold the review and ratings // style the container to that holds the reviews and ratings
+ // style the rating bar?
+ // target the container using querySelector
+// store the review and rating in the database on Firebase // append the review and rating to the page
